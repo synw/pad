7186a86103b73643916143b92fc2f45958420737
synw <synw@yahoo.com>
2019-12-06 07:10:47 +0100

    Add classes

--- lib/src/pad.dart
+++ lib/src/pad.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 class Pad extends StatelessWidget {
   /// Provide a direction
   const Pad(
-      {this.left = 0.0,
+      {Key key,
+      this.left = 0.0,
       this.top = 0.0,
       this.right = 0.0,
       this.bottom = 0.0,
-      this.child});
+      this.child})
+      : super(key: key);
 
   /// Left padding
   final double left;
@@ -44,7 +46,13 @@ class Pad extends StatelessWidget {
 /// A padding from LTRB
 class PadLTRB extends StatelessWidget {
   /// Provide LTRB values
-  const PadLTRB(this.left, this.top, this.right, this.bottom, {this.child});
+  const PadLTRB(this.left, this.top, this.right, this.bottom,
+      {Key key, this.child})
+      : assert(left != null),
+        assert(right != null),
+        assert(top != null),
+        assert(bottom != null),
+        super(key: key);
 
   /// Left padding
   final double left;
@@ -77,10 +85,120 @@ class PadLTRB extends StatelessWidget {
   }
 }
 
+/// Top padding
+class PadTop extends StatelessWidget {
+  /// Provide a [padding]
+  const PadTop(this.padding, {Key key, this.child})
+      : assert(padding != null),
+        super(key: key);
+
+  /// The padding value
+  final double padding;
+
+  /// The child widget
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    Padding p;
+    switch (child == null) {
+      case true:
+        p = Padding(padding: EdgeInsets.only(top: padding));
+        break;
+      case false:
+        p = Padding(padding: EdgeInsets.only(top: padding), child: child);
+    }
+    return p;
+  }
+}
+
+/// Bottom padding
+class PadBottom extends StatelessWidget {
+  /// Provide a [padding]
+  const PadBottom(this.padding, {Key key, this.child})
+      : assert(padding != null),
+        super(key: key);
+
+  /// The padding value
+  final double padding;
+
+  /// The child widget
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    Padding p;
+    switch (child == null) {
+      case true:
+        p = Padding(padding: EdgeInsets.only(bottom: padding));
+        break;
+      case false:
+        p = Padding(padding: EdgeInsets.only(bottom: padding), child: child);
+    }
+    return p;
+  }
+}
+
+/// Right padding
+class PadRight extends StatelessWidget {
+  /// Provide a [padding]
+  const PadRight(this.padding, {Key key, this.child})
+      : assert(padding != null),
+        super(key: key);
+
+  /// The padding value
+  final double padding;
+
+  /// The child widget
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    Padding p;
+    switch (child == null) {
+      case true:
+        p = Padding(padding: EdgeInsets.only(right: padding));
+        break;
+      case false:
+        p = Padding(padding: EdgeInsets.only(right: padding), child: child);
+    }
+    return p;
+  }
+}
+
+/// Left padding
+class PadLeft extends StatelessWidget {
+  /// Provide a [padding]
+  const PadLeft(this.padding, {Key key, this.child})
+      : assert(padding != null),
+        super(key: key);
+
+  /// The padding value
+  final double padding;
+
+  /// The child widget
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    Padding p;
+    switch (child == null) {
+      case true:
+        p = Padding(padding: EdgeInsets.only(left: padding));
+        break;
+      case false:
+        p = Padding(padding: EdgeInsets.only(left: padding), child: child);
+    }
+    return p;
+  }
+}
+
 /// Vertical padding
 class PadVertical extends StatelessWidget {
   /// Provide a [padding]
-  const PadVertical(this.padding, {this.child}) : assert(padding != null);
+  const PadVertical(this.padding, {Key key, this.child})
+      : assert(padding != null),
+        super(key: key);
 
   /// The padding value
   final double padding;
@@ -106,7 +224,9 @@ class PadVertical extends StatelessWidget {
 /// Horizontal padding
 class PadHorizontal extends StatelessWidget {
   /// Provide a [padding]
-  const PadHorizontal(this.padding, {this.child}) : assert(padding != null);
+  const PadHorizontal(this.padding, {Key key, this.child})
+      : assert(padding != null),
+        super(key: key);
 
   /// The padding value
   final double padding;
@@ -132,7 +252,9 @@ class PadHorizontal extends StatelessWidget {
 /// All directions padding
 class PadAll extends StatelessWidget {
   /// Provide a [padding]
-  const PadAll(this.padding, {this.child}) : assert(padding != null);
+  const PadAll(this.padding, {Key key, this.child})
+      : assert(padding != null),
+        super(key: key);
 
   /// The padding value
   final double padding;
